044002d414445ac72304e2f3f3e0f13dbabdd97b
Abdelmonem <abdelmonem.kouki@gmail.com>
2020-05-07 03:33:28 +0200

    ajout exemple put

--- src/main/java/PutExample.java
+++ src/main/java/PutExample.java
@@ -1,5 +1,27 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+// see https://cloud.google.com/bigtable/docs/samples-hbase-java-hello?hl=fr
 public class PutExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Configuration config = HBaseConfiguration.create();
+        //uniquement quant on travail en mode standalone
+        config.addResource("/home/kouki/Documents/hbase-2.2.4/conf/hbase-site.xml");
+        Connection connection = ConnectionFactory.createConnection(config);
+        Table table = connection.getTable(TableName.valueOf("ismahen_table"));
+        String rowKey = "aa" ;
+
+        // Put a single row into the table. We could also pass a list of Puts to write a batch.
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn("personal_data".getBytes(), "name".getBytes(), "ismahan".getBytes());
+        table.put(put);
 
     }
 }
