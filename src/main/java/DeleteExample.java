e88e862e79e06b6540a6373f69a05240140de8bf
Abdelmonem <abdelmonem.kouki@gmail.com>
2020-05-08 02:07:16 +0200

    scan + delete examples

--- src/main/java/DeleteExample.java
+++ src/main/java/DeleteExample.java
@@ -1,5 +1,26 @@
-public class DeleteExample {
-    public static void main(String[] args) {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
+import java.io.IOException;
+
+public class DeleteExample {
+    public static void main(String[] args) throws IOException {
+        Configuration config = HBaseConfiguration.create();
+        //uniquement quant on travail en mode standalone
+        config.addResource("/home/kouki/Documents/hbase-2.2.4/conf/hbase-site.xml");
+        Connection connection = ConnectionFactory.createConnection(config);
+        Table table = connection.getTable(TableName.valueOf("ismahen_table"));
+        Delete delete = new Delete(Bytes.toBytes("rowKey-0"));
+        delete.addColumn(Bytes.toBytes("personal_data"), Bytes.toBytes("name"));
+        // deleting the data
+        table.delete(delete);
+        table.close();
+        connection.close();
     }
 }
--- src/main/java/ScanExample.java
+++ src/main/java/ScanExample.java
@@ -1,2 +1,27 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import java.io.IOException;
+
 public class ScanExample {
+    public static void main(String[] args) throws IOException {
+        Configuration config = HBaseConfiguration.create();
+        //uniquement quant on travail en mode standalone
+        config.addResource("/home/kouki/Documents/hbase-2.2.4/conf/hbase-site.xml");
+        Connection connection = ConnectionFactory.createConnection(config);
+        Table table = connection.getTable(TableName.valueOf("ismahen_table"));
+        Scan scan = new Scan();
+
+        System.out.println("Scan for all rows:");
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result row : scanner) {
+            byte[] valueBytes = row.getValue("personal_data".getBytes(), "name".getBytes());
+            System.out.println('\t' + Bytes.toString(valueBytes));
+        }
+        table.close();
+        connection.close();
+    }
+
 }
